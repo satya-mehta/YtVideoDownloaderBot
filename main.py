be5d15c7c81c65776cefefbbf718fb22afa533f8
Satya Mehta <satyamehta577@gmail.com>
2025-06-06 13:10:09 +0530

    adding a webhook service

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import yt_dlp
 
-def download_youtube_video(video_url, save_path="downloads"):
+
+def download_youtube_video(video_url, save_path="downloads", audvid=True):
     ydl_opts = {
-        'format': 'bestvideo+bestaudio/best',  # Download best video and audio and merge
+        'format': 'bestvideo+bestaudio/best' if audvid else 'bestaudio/best',  # Download best video and audio and merge
         'outtmpl': f'{save_path}/%(title)s.%(ext)s',  # Save file format
-        'merge_output_format': 'mp4',  # Merge video and audio into MP4 format
+        #'merge_output_format': 'mp4' if audvid else 'mp3',  # Merge video and audio into MP4 format
         'noplaylist': True, # Only download the single video, not the whole playlist if it's part of one
     }
+    if audvid:
+        ydl_opts['merge_output_format'] = 'mp4'  # Ensure video + audio is merged into MP4
+    else:
+        ydl_opts['postprocessors'] = [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',  # Convert to MP3
+            'preferredquality': '320',  # Set bitrate quality
+        }]
+
     try:
         print(f"Downloading: {video_url}")
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -15,6 +25,7 @@ def download_youtube_video(video_url, save_path="downloads"):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-if __name__ == "__main__":
-    video_url = input("Enter the YouTube video URL: ")
-    download_youtube_video(video_url)
+# if __name__ == "__main__":
+#     video_url = input("Enter the YouTube video URL: ")
+
+#     download_youtube_video(video_url, audvid=False)
---
+++ server.py
@@ -0,0 +1,43 @@
+import os
+import flask
+import requests
+import main
+
+from flask import request
+
+Token = "7150884626:AAGMo7q4Rg-o9wDxo5PwI-Cwz46ip9rBc5M"
+WEBHOOK_URL = ""
+
+app = flask.Flask(__name__)
+
+def set_webhook():
+    url = f"https://api.telegram.org/bot{Token}/setWebhook?url={WEBHOOK_URL}"
+    response = requests.get(url)
+    return response.json()
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Bot is running!"
+
+@app.route("/webhook", method=["POST"])
+def webhook():
+    data = request.json
+
+    if "message" in data:
+        chat_id = data["message"]["chat"]["id"]
+        text = data["message"]["text"]
+
+        if text.stratswith("https://www.youtube.com/"):
+            video_url = text
+            send_message(chat_id, "Download started!")
+            main.download_youtube_video(video_url)
+            send_message(chat_id, "Download complete!")
+    return "OK"
+
+def send_message(chat_id, text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(url, json={"chat_id": chat_id,"text": text})
+
+if __name__ == "__main__":
+    set_webhook()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
